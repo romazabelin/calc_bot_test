24f77cd337a3200089a3f00016ea492c93c0b663
roma <romazabelin1991@gmail.com>
2019-11-07 23:52:49 +0200

    keyboard

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -9,6 +9,17 @@ class BotController extends Controller
 {
     public function webhookHandler()
     {
+        $update = $this->getWebhookUpdates();
+        $callback_query = $update->getCallbackQuery()->getData();
+
+        if ($update->getCallbackQuery->getId()) {
+            Telegram::answerCallbackQuery([
+                'text' => $callback_query,
+                'callback_query_id' => $update->getId(),
+                'show_alert' => true
+            ]);
+        }
+
         Telegram::commandsHandler(true);
 
 //        $update = Telegram::getWebhookUpdates();
