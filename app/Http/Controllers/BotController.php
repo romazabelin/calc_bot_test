6c285f8aa029a33f61237425e7a683b3368afe7b
roma <romazabelin1991@gmail.com>
2019-11-07 23:55:46 +0200

    keyboard

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -10,30 +10,29 @@ class BotController extends Controller
     public function webhookHandler()
     {
         Telegram::commandsHandler(true);
-        $update = $this->getWebhookUpdates();
-        $callback_query = $update->getCallbackQuery()->getData();
-
-        if ($update->getCallbackQuery()->getId()) {
-            Telegram::answerCallbackQuery([
-                'text' => $callback_query,
-                'callback_query_id' => $update->getId(),
-                'show_alert' => true
-            ]);
-        }
-
-//        $update = Telegram::getWebhookUpdates();
-//        $telegramUser = $update['message'];
-//        $query = $update->getCallbackQuery();
-
-//        if ($query->getId()) {
-//            Telegram::sendMessage([
-//                'text' => 'dsdsds'
-//            ]);
+//        $update = $this->getWebhookUpdates();
+//        $callback_query = $update->getCallbackQuery()->getData();
+//
+//        if ($update->getCallbackQuery()->getId()) {
 //            Telegram::answerCallbackQuery([
-//                'text' => $query->getId(),
-//                'callback_query_id' => $query->getId(),
+//                'text' => $callback_query,
+//                'callback_query_id' => $update->getId(),
 //                'show_alert' => true
 //            ]);
 //        }
+
+        $update = Telegram::getWebhookUpdates();
+        $query = $update->getCallbackQuery();
+
+        if ($query->getId()) {
+            Telegram::sendMessage([
+                'text' => 'dsdsds'
+            ]);
+            Telegram::answerCallbackQuery([
+                'text' => $query->getData(),
+                'callback_query_id' => $query->getId(),
+                'show_alert' => true
+            ]);
+        }
     }
 }
