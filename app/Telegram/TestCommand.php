35027a27e5c06eaa8d305f9473b7cc3c54f94ba1
roma <romazabelin1991@gmail.com>
2019-11-08 11:34:37 +0200

    answer callback query

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -5,7 +5,6 @@ namespace App\Telegram;
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 use Telegram\Bot\Keyboard\Keyboard;
-use Telegram;
 
 class TestCommand extends Command
 {
@@ -48,11 +47,11 @@ class TestCommand extends Command
                 Keyboard::inlineButton(['text' => '/', 'callback_data' => 'sdsdsds'])
             );
 
-//        $this->replyWithMessage(['text' => $update->getMessage()->getChat()->getId(), 'reply_markup' => $keyboard]);
-        Telegram::sendMesage([
-            'chat_id' => $update->getMessage()->getChat()->getId(),
-            'text' => 'sdsdsds'
-        ]);
+        $this->replyWithMessage(['text' => $update->getMessage()->getChat()->getId(), 'reply_markup' => $keyboard]);
+//        Telegram::sendMesage([
+//            'chat_id' => $update->getMessage()->getChat()->getId(),
+//            'text' => 'sdsdsds'
+//        ]);
 
         //query->getMessage()->getChat()->getId()
 
