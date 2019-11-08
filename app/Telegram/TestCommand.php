1775e6eae18117caa6be7264d53eb1d0c46448ba
roma <romazabelin1991@gmail.com>
2019-11-08 12:30:56 +0200

    answer callback query

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -29,7 +29,8 @@ class TestCommand extends Command
      */
     public function handle($arguments)
     {
-        $update = Telegram::getWebhookUpdates();
+//        $update = Telegram::getWebhookUpdates();
+        $update = $this->getUpdate();
 
         $keyboard = Keyboard::make()
             ->inline()
@@ -48,11 +49,11 @@ class TestCommand extends Command
                 Keyboard::inlineButton(['text' => '/', 'callback_data' => 'sdsdsds'])
             );
 
-//        $this->replyWithMessage(['text' => $update->getChat()->getId(), 'reply_markup' => $keyboard]);
-        Telegram::sendMesage([
-            'chat_id' => $update->getChat()->getId(),
-            'text' => 'sdsdsds'
-        ]);
+        $this->replyWithMessage(['text' => $update->getChat()->getId(), 'reply_markup' => $keyboard]);
+//        Telegram::sendMesage([
+//            'chat_id' => $update->getChat()->getId(),
+//            'text' => 'sdsdsds'
+//        ]);
 
         //triggerCommand
         //$update->getMessage()->getChat()->getId()
