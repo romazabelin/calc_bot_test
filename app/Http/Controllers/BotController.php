0bc06e407e8dbc36e97fd395df51e5f59a8cd9a8
roma <romazabelin1991@gmail.com>
2019-11-08 11:21:51 +0200

    answer callback query

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -20,15 +20,15 @@ class BotController extends Controller
         $query = $update->getCallbackQuery();
 
         if ($update->isType('callback_query') && $query->getId()) {
-//            Telegram::sendMesage([
-//                'chat_id' => $query->getId(),
-//                'text' => 'sdsdsds'
-//            ]);
-            Telegram::answerCallbackQuery([
-                'text' => $query->getData() . ' ' . $query->getFrom()->getId() . ' ' . $query->getId(),
-                'callback_query_id' => $query->getId(),
-                'show_alert' => true
+            Telegram::sendMesage([
+                'chat_id' => $query->getFrom()->getId(),
+                'text' => 'sdsdsds'
             ]);
+//            Telegram::answerCallbackQuery([
+//                'text' => $query->getData() . ' ' . $query->getFrom()->getId() . ' ' . $query->getId(),
+//                'callback_query_id' => $query->getId(),
+//                'show_alert' => true
+//            ]);
         } else {
             Telegram::commandsHandler(true);
         }
