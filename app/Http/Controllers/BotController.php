2daa5a2dd204566f13204becce5b66c962f42f37
roma <romazabelin1991@gmail.com>
2019-11-08 16:18:53 +0200

    try edit message

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -20,30 +20,35 @@ class BotController extends Controller
 //                'callback_query_id' => $query->getId(),
 //                'show_alert' => true
 //            ]);
-            $keyboard = Keyboard::make()
-                ->inline()
-                ->row(
-                    Keyboard::inlineButton(['text' => '1', 'callback_data' => '1']),
-                    Keyboard::inlineButton(['text' => '2', 'callback_data' => '2']),
-                    Keyboard::inlineButton(['text' => '3', 'callback_data' => '3']),
-                    Keyboard::inlineButton(['text' => '+', 'callback_data' => '+']),
-                    Keyboard::inlineButton(['text' => '-', 'callback_data' => '-'])
-                )
-                ->row(
-                    Keyboard::inlineButton(['text' => '4', 'callback_data' => '6']),
-                    Keyboard::inlineButton(['text' => '5', 'callback_data' => '7']),
-                    Keyboard::inlineButton(['text' => '6', 'callback_data' => '8']),
-                    Keyboard::inlineButton(['text' => '*', 'callback_data' => '*']),
-                    Keyboard::inlineButton(['text' => '/', 'callback_data' => '/'])
-                )
-                ->row(
-                    Keyboard::inlineButton(['text' => $query->getData(), 'callback_data' => 'result'])
-                );
 
-            Telegram::sendMessage([
-                'chat_id' => $query->getFrom()->getId(),
-                'text' => $query->getData(),
-                'reply_markup' => $keyboard
+//            $keyboard = Keyboard::make()
+//                ->inline()
+//                ->row(
+//                    Keyboard::inlineButton(['text' => '1', 'callback_data' => '1']),
+//                    Keyboard::inlineButton(['text' => '2', 'callback_data' => '2']),
+//                    Keyboard::inlineButton(['text' => '3', 'callback_data' => '3']),
+//                    Keyboard::inlineButton(['text' => '+', 'callback_data' => '+']),
+//                    Keyboard::inlineButton(['text' => '-', 'callback_data' => '-'])
+//                )
+//                ->row(
+//                    Keyboard::inlineButton(['text' => '4', 'callback_data' => '6']),
+//                    Keyboard::inlineButton(['text' => '5', 'callback_data' => '7']),
+//                    Keyboard::inlineButton(['text' => '6', 'callback_data' => '8']),
+//                    Keyboard::inlineButton(['text' => '*', 'callback_data' => '*']),
+//                    Keyboard::inlineButton(['text' => '/', 'callback_data' => '/'])
+//                )
+//                ->row(
+//                    Keyboard::inlineButton(['text' => $query->getData(), 'callback_data' => 'result'])
+//                );
+//
+//            Telegram::sendMessage([
+//                'chat_id' => $query->getFrom()->getId(),
+//                'text' => $query->getData(),
+//                'reply_markup' => $keyboard
+//            ]);
+            Telegram::editMessageText([
+                'message_id' => 463,
+                'text' => 'Update me'
             ]);
         } else {
             Telegram::commandsHandler(true);
